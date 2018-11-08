91b67cbdd6c5d38328448b4864bd16cd060043e5
Jon Sherrard <jon.sherrard@shortlist.com>
2018-11-08 14:59:29 +0000

    what was I doing

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,6 @@
 import 'make-promises-safe'
-import Hapi from 'hapi'
 
 import DynamicRouteHandler from './handlers/dynamic'
-
 import CacheManager from './utilities/cache-manager'
 
 // Create CacheManager Singleton
@@ -11,53 +9,16 @@ new CacheManager()
 class Server {
   constructor({ config }) {
     // Create Hapi server instance
-    const server = Hapi.server({
-      host: process.env.HOST || '0.0.0.0',
-      port: process.env.PORT || 3000,
-      router: {
-        isCaseSensitive: false,
-        stripTrailingSlash: true
-      },
-      routes: {
-        security: {
-          hsts: true,
-          noOpen: true,
-          noSniff: true,
-          xframe: false,
-          xss: true
-        }
-      }
+    // Require the framework and instantiate it
+    const server = require('fastify')({
+      logger: true
     })
 
-    server.ext('onPreResponse', ({ response }, h) => {
-      // isServer indicates status code >= 500
-      // if error, pass it through server.log
-      if (response && response.isBoom && response.isServer) {
-        console.error(response.error || response.message)
-      }
-      // The important bit
-      if (response.headers) response.headers['X-Powered-By'] = 'Tapestry'
-      return h.continue
-    })
     // Handle server routes
     DynamicRouteHandler({ config, server })
     // return server instance (not class)
     return server
   }
-}
-
-export const registerPlugins = async ({ server, config }) => {
-  // Handles static files and proxying
-  const plugins = [require('inert'), require('h2o2')]
-  // Redirects all internal requests to https
-  if (config.forceHttps) {
-    plugins.push({
-      register: require('hapi-require-https'),
-      options: { proxy: false }
-    })
-  }
-  // Register all required plugins before use
-  await server.register(plugins)
 }
 
 export default Server
